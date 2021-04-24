a4bd8dce7586ebcd947067d613df0be2115c290e
kollead <kollid33@gmail.com>
2021-04-24 23:58:44 +0900

    menu and loggin

--- front/component/AppLayout.js
+++ front/component/AppLayout.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link'
-import {Input, Menu} from 'antd'
+import {Input, Menu, Row, Col} from 'antd'
 
 const AppLayout = ({children}) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
  return (
      <div>
          <Menu mode="horizontal">
@@ -14,13 +15,23 @@ const AppLayout = ({children}) => {
                 <Link href="/profile"><a>profile</a></Link>
              </Menu.Item>
              <Menu.Item>
-                 <Input.Search/>
+                 <Input.Search enterButton style={{verticalAlign: 'middle'}}/>
              </Menu.Item>
              <Menu.Item>
                 <Link href="/signup"><a>signup</a></Link>
              </Menu.Item>
          </Menu>
-         {children}
+         <Row gutter={8}>
+             <Col xs={24} md={6} >
+                 {isLoggedIn ? <UserProfile/> : <LoginForm/>}
+             </Col>
+             <Col xs={24} md={12} >                 
+                {children}
+             </Col>
+             <Col xs={24} md={6} >
+                <a href="" target="_blank" rel="noreferrer noopener">Sujin Bae</a>
+             </Col>
+         </Row>
      </div>
  );
 };
