99fc5f4adf6d4dbd53b00f3c86dd0359e0dfe476
kollead <kollid33@gmail.com>
2021-04-28 23:56:32 +0900

    profileComponent

--- front/component/NickNameEditForm.js
+++ front/component/NickNameEditForm.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {useMemo} from 'react'
+import {Form, Input} from 'antd'
+import PropTypes from 'prop-types'
 
-function NickNameEditForm() {
+function NickNameEditForm({header, data}) {
+
+    const style = useMemo(() => ({
+        marginBottom: '20px', border: '1px solid #d9d9d9', padding: '10px'
+    }), [])
+
     return (
-        <div>
-            
-        </div>
+        <Form style={style}>
+            <Input.Search addonBefore="nickName" enterButton="Modify"/>
+        </Form>
     )
+}
+
+NickNameEditForm.propTypes = {
+    header: PropTypes.string.isRequired,
+    data: PropTypes.array.isRequired
 }
 
 export default NickNameEditForm
--- front/pages/profile.js
+++ front/pages/profile.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import AppLayout from '../component/AppLayout'
 import Head from 'next/head'
+import NickNameEditForm from '../component/NickNameEditForm'
+import FollowList from '../component/FollowingList'
 
 const Profile = () => {
+    const followerList = [{nickname: "dummy01"},{nickname: "dummy02"},{nickname: "dummy03"}]
+    const followingList = [{nickname: "dummy01"},{nickname: "dummy02"},{nickname: "dummy03"}]
     return (
         <>
             <Head>
@@ -11,7 +15,7 @@ const Profile = () => {
             <AppLayout>
                 <NickNameEditForm/>
                 <FollowList header="Following List" data={followingList}/>
-                <FollowerList header="Follower List" data={followerList}/>
+                <FollowList header="Follower List" data={followerList}/>
             </AppLayout>
         </>
     )
