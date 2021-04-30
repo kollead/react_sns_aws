fd219ec1da2b4a51cf499fe66c9fa27a2f836c61
kollead <kollid33@gmail.com>
2021-04-30 23:58:31 +0900

    nextRedux

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,29 +1,50 @@
 import React, {useCallback, useState} from 'react'
 import AppLayout from '../component/AppLayout'
 import Head from 'next/head'
-import {Form, Input} from 'antd'
+import {Form, Input, Checkbox, Button} from 'antd'
 import useInput from '../hooks/useInput'
+import Password from 'antd/lib/input/Password'
+import styled from 'styled-components'
+
+const ErrorMessage = styled.div`
+    color: red;
+`;
 
 const Signup = () => {
 
     const [id, onChangeId] = useInput('')
     const [nickname, onChangeNickname] = useInput('')
     const [password, onChangePassword] = useInput('')
+ 
     const [passwordCheck, setPasswordCheck] = useState('')
     const [passwordError, setPasswordError] = useState(false)
-    
-    const onChangePasswordCheck = useCallback(
+     const onChangePasswordCheck = useCallback(
         (e) => {
           setPasswordCheck(e.target.value);
           setPasswordError(e.target.value!==password);  
-        },[password],
+        }, [password],
     );
+
+    const [term, setTerm] = useState('')
+    const [termError, setTermError] = useState(false)
+    const onChangeTerm = useCallback(
+        (e) => {
+            setTerm(e.target.checked);
+            setTermError(false);
+        }, []
+    )
     
     
     const onsubmit = useCallback(
         () => {
-            
-        },[],
+            if (password !== passwordCheck) {
+                return setPasswordError(true);
+            }
+            if (! term) {
+                return setTermError(true);
+            }
+            console.log(id, nickname, passwordCheck)
+        },[password, passwordCheck, term]
     );
 
     return( 
@@ -38,22 +59,33 @@ const Signup = () => {
                         <Input name="user-id" value={id} required onChange={onChangeId}/>
                     </div>
                     <div>
-                        <label htmlFor="user-id">Id</label>
+                        <label htmlFor="user-nickname">Id</label>
                         <br/>
-                        <Input name="user-id" value={id} required onChange={onChangeId}/>
+                        <Input name="user-nickname" value={nickname} required onChange={onChangeNickname}/>
+                    </div>
+                    <div>
+                        <label htmlFor="user-password">Id</label>
+                        <br/>
+                        <Input name="user-password" type="password" value={Password} required onChange={onChangePassword}/>
                     </div>
                     <div>
                         <label htmlFor="user-id">Id</label>
                         <br/>
-                        <Input name="user-id" value={id} required onChange={onChangeId}/>
+                        <Input 
+                            name="user-password-check" 
+                            type="password"
+                            value={passwordCheck} 
+                            required 
+                            onChange={onChangePasswordCheck}/>
+                        {passwordError && <ErrorMessage>Password does not match</ErrorMessage>}
                     </div>
                     <div>
-                        <label htmlFor="user-id">Id</label>
-                        <br/>
-                        <Input name="user-id" value={id} required onChange={onChangeId}/>
-                        {passwordError && <div></div>}
+                        <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>Be nice</Checkbox>
+                        {termError && <ErrorMessage>You need to agree the statement</ErrorMessage>}
                     </div>
-
+                    <div style={{marginTop: 10}}>
+                        <Button type="primary" htmlType="submit">Sign up</Button>
+                    </div>
                 </Form>
             </AppLayout>
             
