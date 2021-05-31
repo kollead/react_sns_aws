15382f60e3a60a38187cbabd5b9c3a93b96fb5dc
kollead <kollid33@gmail.com>
2021-05-31 23:42:44 +0900

    eslint modify

--- front/component/CommentForm.js
+++ front/component/CommentForm.js
@@ -1,13 +1,24 @@
-import React, {useCallback} from 'react'
+import React, {useCallback, useEffect} from 'react'
 import {Form, Input, Button} from 'antd'
 import PropTypes from 'prop-types'
 import useInput from '../hooks/useInput'
 import { useDispatch, useSelector } from 'react-redux'
 import {ADD_COMMENT_REQUEST} from '../reducers/post'
+
+
 function CommentForm({post}) {
     const dispatch = useDispatch();
-    const [commentText, onChangeCommentText] = useInput
+    const [commentText, onChangeCommentText,setCommentText] = useInput
     const id = useSelector((state)=>state.user.user?.id)
+    const {addCommentDone} = useSelector((state)=>state.post.addCommentDone)
+
+    useEffect(() => {
+        if(addCommentDone){
+            setCommentText('')
+        }
+        
+    }, [addCommentDone])
+    
     const onSubmitComment = useCallback(
         () => {
             console.log(post.id, commentText)
--- front/component/PostForm.js
+++ front/component/PostForm.js
@@ -1,64 +1,63 @@
-import React, {useCallback, useState, useRef, useEffect} from 'react'
-import {Form, Input, Button} from 'antd'
-import {useDispatch, useSelector} from 'react-redux'
-import {addPost} from '../reducers/post'
+import React, {useCallback, useRef, useEffect} from 'react';
+import { Form, Input, Button} from 'antd';
+import {useDispatch, useSelector} from 'react-redux';
+import {addPost} from '../reducers/post';
+import useInput from '../hooks/useInput';
+
 function PostForm() {
+  const dispatch = useDispatch();
+  const [text, onChangeText, SetText] = useInput();
+  const {imagePaths, addPostDone} = useSelector((state) => state.post);
+  const imageInput = useRef();
 
-    const dispatch = useDispatch()
-    const [text, SetText] =  useState("")
-    const {imagePaths, addPostDone} = useSelector(state => state.post)
-    const imageInput = useRef()
-    useEffect(() => {
-        if(addPostDone){
-            SetText('')
-        }
-    }, [addPostDone])
+  useEffect(() => {
+    if (addPostDone) {
+      SetText('');
+    }
+  }, [addPostDone]);
 
-    const onSubmit = useCallback(
-        () => {
-            dispatch(addPost(text))
-            
-        },
-        [text],
-    )
-    const onChangeText =useCallback(
-        (e) => {
-          SetText(e.target.value)  
-        },
-        [],
-    )
-    const onClickImageUpload = useCallback(
-        () => {
-            imageInput.current.click()   
-        },
-        [imageInput.current],
-    )
+  const onSubmit = useCallback(
+    () => {
+      dispatch(addPost(text));
+    },
+    [text],
+  );
+  const onClickImageUpload = useCallback(
+    () => {
+      imageInput.current.click();
+    },
+    [imageInput.current],
+  );
 
-    return (
-        <Form style={{margin: '10px 0 20px'}} encType="multipart/form-data" onFinish={onSubmit}>
-            <Input.TextArea 
-             value={text}
-             onChange={onChangeText}
-             maxLength={140}
-             placeholder="Share your story"/>
-            <div>
-                <input type="file" multiple hidden ref={imageInput}/>
-                <Button onClick={onClickImageUpload}>Image Upload</Button>
-                <Button 
-                 type="primary" 
-                 style={{float: 'right'}}
-                 htmlType="submit">Twit</Button>
-            </div>
-            <div>
-                {imagePaths&&imagePaths.map((v)=>(
-                    <div key={v} style={{display: 'inline-block'}}>
-                        <img src={v} style={{width: '200px'}} alt={v}/>
-                        <div><Button>Remove</Button></div>
-                    </div>
-                ))}
-            </div>
-        </Form>
-    )
+  return (
+    <Form style={{margin: '10px 0 20px'}} encType="multipart/form-data" onFinish={onSubmit}>
+      <Input.TextArea
+        value={text}
+        onChange={onChangeText}
+        maxLength={140}
+        placeholder="Share your story"
+      />
+      <div>
+        <input type="file" multiple hidden ref={imageInput}/>
+        <Button onClick={onClickImageUpload}>Image Upload</Button>
+        <Button
+          type="primary"
+          style={{float: 'right'}}
+          htmlType="submit"
+        >
+          Twit
+        </Button>
+      </div>
+      <div>
+        {imagePaths && imagePaths.map((v) => (
+          <div key={v} style={{display: 'inline-block'}}>
+            <img src={v} style={{width: '200px'}} alt={v} />
+            <div><Button>Remove</Button></div>
+          </div>
+        ))}
+      </div>
+    </Form>
+  );
 }
 
-export default PostForm
+export default PostForm;
