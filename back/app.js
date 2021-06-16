be079a69a4b6beba989251af04121a891fb466a0
kollead <kollid33@gmail.com>
2021-06-16 23:52:04 +0900

    express route

---
+++ back/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello express');
+});
+app.listen(3065, () => {
+  console.log('서버 실행 중...');
+});
