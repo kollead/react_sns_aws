d59fcaac08c91ae7de053e59cdc67d869862f2cb
kollead <kollid33@gmail.com>
2021-06-19 03:04:33 +0900

    routes and mysql

--- back/app.js
+++ back/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const postRouter = require('./routes/post');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello express');
-});
+app.use('/post', postRouter);
 app.listen(3065, () => {
   console.log('서버 실행 중...');
 });
