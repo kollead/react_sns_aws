44534440be726c9a38d246ebe001012ec16574b5
kollead <kollid33@gmail.com>
2021-06-19 23:58:34 +0900

    config and sequel

---
+++ back/models/index.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
+
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
