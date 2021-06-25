e1050318402e5ededf3f79b3c9f7d9ccd544da7e
kollead <kollid33@gmail.com>
2021-06-26 00:00:03 +0900

    bcrypt

--- back/app.js
+++ back/app.js
@@ -3,13 +3,19 @@ const postRouter = require('./routes/post');
 const userRouter = require('./routes/');
 const db = require('./models');
 const app = express();
+
 db.sequelize.sync()
   .then(() => {
     console.log('db 연결 성공');
   })
   .catch(console.error); 
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.use('/post', postRouter);
 app.use('/user', userRouter);
+
 app.listen(3065, () => {
   console.log('서버 실행 중...');
 });
