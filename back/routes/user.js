fa66c792bb5ef28bbc6ad822469b317a83ad9c20
kollead <kollid33@gmail.com>
2021-06-27 23:59:00 +0900

    user routes

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,14 +1,29 @@
 const express =  require('express');
+const bcrypt = require('bcrypt');
 const { User } = require('../models');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    await User.create({
+router.post('/', async (req, res, next) => {
+  try {
+    const exUser = await User.findOne({
+      where: {
         email: req.body.email,
-        nickname: req.body.nickname,
-        password: req.body.password,
+      }
     });
-    res.json();
+    if(exUser) {
+      return res.status(403).send('사용중인 이메일입니다');
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    await User.create({
+      email: req.body.email,
+      nickname: req.body.nickname,
+      password: req.body.password,
+  });
+  res.send('ok');
+  } catch (error) {
+      console.error(error);
+      next(error);
+  }
 });
 module.exports = router;
