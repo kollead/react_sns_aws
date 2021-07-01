3fc7a41043425de2528c072bd7b43f5340197c51
kollead <kollid33@gmail.com>
2021-07-02 02:13:16 +0900

    gitignore

--- back/config/config.js
+++ back/config/config.js
@@ -11,14 +11,14 @@ module.exports = {
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "react-sns",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": process.env.DB_PASSWORD,
     "database": "react-sns",
     "host": "127.0.0.1",
     "dialect": "mysql"
