341c01e6947f2ba4ee7672c445fe9ff96b580204
kollead <kollid33@gmail.com>
2021-09-05 00:34:30 +0900

    process env and add dependencies

--- back/app.js
+++ back/app.js
@@ -5,6 +5,10 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const passport = require('passport')
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const path = require('path');
+const hpp = require('hpp');
+const helmet = require('helmet');
 
 const postRouter = require('./routes/post');
 const postsRouter = require('./routes/posts');
@@ -24,8 +28,16 @@ db.sequelize.sync()
 
 passportConfig();
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan('dev'));
+}
+
 app.use(cors({
-  origin: true,
+  origin: ['http://localhost:3060', 'reactNodeSns.com'],
   credentials: true,
 }));
 app.use('/', express.static(path.join(__dirname, 'uploads')));
